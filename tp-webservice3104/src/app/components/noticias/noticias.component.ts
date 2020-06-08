4841711c822266f140f273e982ffc700ef8ad3b7
enzoaramayo <50436437+enzoaramayo@users.noreply.github.com>
2020-06-08 00:06:46 -0300

    commmit

--- tp-webservice3104/src/app/components/noticias/noticias.component.ts
+++ tp-webservice3104/src/app/components/noticias/noticias.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Injectable } from '@angular/core';
+import { Noticia } from './../../models/noticia';
+import { NoticiaService } from './../../services/noticia.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-noticias',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoticiasComponent implements OnInit {
 
-  constructor() { }
+  noticia: Noticia;
+  noticias: Array<Noticia>;
+  categoria: string;
+  subscription: any;
 
-  ngOnInit(): void {
+
+  constructor(private noticiaService: NoticiaService, public rutaActiva: ActivatedRoute) {
+    this.getNoticias();
   }
 
+  ngOnInit() {
+
+  }
+
+
+  getNoticias() {
+
+    this.noticiaService.getAllNews('soccer').subscribe(
+      (result) => {
+        this.noticias = new Array<Noticia>();
+        // tslint:disable-next-line: no-string-literal
+        result['arts'].forEach(element => {
+          this.noticia = new Noticia();
+          Object.assign(this.noticia, element);
+          this.noticias.push(this.noticia);
+        });
+      },
+      error => { alert('Error en la petición'); }
+    );
+  }
 }
+
