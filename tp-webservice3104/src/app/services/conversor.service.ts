b7425b60fd7dc37d4af43392e0ae4b62d8b86a88
enzoaramayo <50436437+enzoaramayo@users.noreply.github.com>
2020-06-07 22:57:31 -0300

    commit

--- tp-webservice3104/src/app/services/conversor.service.ts
+++ tp-webservice3104/src/app/services/conversor.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ɵɵresolveBody } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Conversion } from '../models/conversion';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConversorService {
 
-  constructor() { }
+  monedas: Array<any>;
+
+  constructor(private _http: HttpClient) {
+    this.monedas = [
+      { 
+      codigoMoneda: 'USD', imagen: './../../../assets/conversor/usd.png' },
+    { codigoMoneda: 'EUR', imagen: './../../../assets/conversor/eur.png' },
+    { codigoMoneda: 'JPY', imagen: './../../../assets/conversor/jpy.png' },
+    { codigoMoneda: 'GBP', imagen: './../../../assets/conversor/gbp.png' },
+    { codigoMoneda: 'CAD', imagen: './../../../assets/conversor/cad.png' },
+    { codigoMoneda: 'AUD', imagen: './../../../assets/conversor/aud.png' },
+    { codigoMoneda: 'CHF', imagen: './../../../assets/conversor/chf.png' },
+    { codigoMoneda: 'HKD', imagen: './../../../assets/conversor/hkd.png' },
+    { codigoMoneda: 'SEK', imagen: './../../../assets/conversor/sek.png' },
+    { codigoMoneda: 'NZD', imagen: './../../../assets/conversor/nzd.png' }
+    ];
+  }
 }
